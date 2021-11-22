cf29a6502c0a89217efd2ce575de22b4676c9430
Ian <Ian.Hinden@gmail.com>
2021-11-21 18:09:53 -0800

    Work on setting up multiple pages and an initial create post form

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
     return <nav className="navbar bg-dark container">
-        <h4><Link to="/">Home</Link></h4>
-        <h4><Link to="/posts">Posts</Link></h4>
-        <h4><Link to="/create">Create</Link></h4>
+        <h4><Link className="link" to="/">Home</Link></h4>
+        <h4><Link className="link" to="/posts">Posts</Link></h4>
+        <h4><Link className="link" to="/create">Create</Link></h4>
     </nav>
 }
 
--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreatePost() {
+    const [input, setInput] = useState({
+        title: '',
+        content: ''
+    })
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setInput(prevInput => {
+            return {
+                ...prevInput,
+                [name]: value
+            }
+        })
+    }
+
+    function handleClick(event) {
+        event.preventDefault();
+        console.log(input);
+    }
+
     return <div className="container">
         <h1>Create Post Page</h1>
+        <form>
+            <div className='form-group'>
+                <input onChange={handleChange} name="title" value={input.title} autoComplete="off" className='form-control' placeholder="Post Title"></input>
+            </div>
+
+            <div className='form-group'>
+                <textarea onChange={handleChange} name="content" value={input.content} autoComplete="off" className='form-control' placeholder="Post Content"></textarea>
+            </div>
+
+            <button onClick={handleClick} className="btn btn-lg btn-info">Add Post</button>
+        </form>
     </div>
 }
 
