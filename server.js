1f95bb10d8e87c2d15ca965cda9bede3272e81c7
Ian <Ian.Hinden@gmail.com>
2021-11-26 20:14:33 -0800

    Testing local host in pipeline

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require("path");
-const cors = require ('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config()
 
@@ -10,6 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("Connection opened");
+});
 
 app.use('/', require('./routes/postsRoute'));
 
