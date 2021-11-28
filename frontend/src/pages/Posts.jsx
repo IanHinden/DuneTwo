3cdcfaa02d005c228366522cb9780b35dc23f9d1
Ian <Ian.Hinden@gmail.com>
2021-11-28 13:24:57 -0800

    Display results to screen

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+//import { getAllPosts } from "../../../controllers/postsController";
 
 function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        getAllPosts();
+    }, []);
+
+    const getAllPosts = () => {
+        return axios
+            .get('http://localhost:5000/posts')
+            .then((res) => {
+                setPosts(res.data);
+                console.log(res.data);
+                console.log("Got em", posts)
+            })
+            .catch((err) => console.log(err));
+    }
+
     return <div className="container">
         <h1>Posts</h1>
+        <ul>
+            {posts.map((post) => (
+                <li key={post.title}>
+                    title: {post.title}
+                    content: {post.content}
+                    <hr />
+                    <br />
+                </li>
+            ))}
+        </ul>
     </div>
 }
 
