e90c3bce4f94db781a1d9066daefb7e7fc0fb1f5
Ian <Ian.Hinden@gmail.com>
2021-12-02 21:26:10 -0800

    Working react function example

--- frontend/src/components/PostCard.js
+++ frontend/src/components/PostCard.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import './PostCard.css';
 
 function PostCard(props) {
-    return <div class="card">
-      <p>
-        {props.title}
-      </p>
-  </div>
+	const [count, setCount] = useState(10);
+
+		return <div class="card">
+			<p>
+				{props.title}
+			</p>
+			<p>{count}</p>
+			<button onClick={() => setCount(count + 1)}>
+				+
+			</button>
+	</div>
 }
 
 export default PostCard;
