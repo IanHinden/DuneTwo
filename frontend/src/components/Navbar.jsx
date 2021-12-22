a2c1f57c9ce63d8c5e511c9ee09cb3aedaca4c3f
Ian <Ian.Hinden@gmail.com>
2021-12-21 22:53:05 -0800

    Styled navbar / Added jquery and popper

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTwitter, faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons"
+import { faTwitter, faGithub, faInstagram, faGooglePlus } from "@fortawesome/free-brands-svg-icons"
 import "./Navbar.css";
 
 function Navbar() {
     return <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
         <div className="container">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <span className="navbar-text">Help Me Get Into Dune 2</span>
+                    </li>
+                </ul>
                 <ul className="navbar-nav mx-auto">
                     <li className="nav-item">
                         <Link className="nav-link" to="/">Home</Link>
@@ -27,6 +32,12 @@ function Navbar() {
                     <li className="nav-item"><a className="nav-link" href="https://twitter..com/orphanshow"><FontAwesomeIcon icon={faTwitter} /></a></li>
                     <li className="nav-item"><a className="nav-link" href="https://github.com/IanHinden"><FontAwesomeIcon icon={faGithub} /></a></li>
                     <li className="nav-item"><a className="nav-link" href="https://instagram.com/IanHinden"><FontAwesomeIcon icon={faInstagram} /></a></li>
+                    <li className="nav-item"><a className="nav-link" href="https://instagram.com/IanHinden"><FontAwesomeIcon icon={faGooglePlus} /></a></li>
+                </ul>
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <button type="button" className="btn btn-outline-primary">Login</button>
+                    </li>
                 </ul>
             </div>
         </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
