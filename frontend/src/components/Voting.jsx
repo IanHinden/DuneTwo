714fd8e3d5b20749ae660bd3633aac2e1792fa92
Ian <Ian.Hinden@gmail.com>
2022-01-18 22:19:45 -0800

    Added ability to vote and UI to show vote status

--- frontend/src/components/Voting.jsx
+++ frontend/src/components/Voting.jsx
@@ -2,19 +2,31 @@ import React, { useState, useContext, useEffect } from "react";
 import {Button, Label} from 'react-bootstrap';
 import axios from "axios";
 import LoginModal from './LoginModal';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { UserContext } from "../UserContext";
 import './Voting.css';
 
 function Voting(props) {
     const {user, setUser} = useContext(UserContext);
     const [show, setShow] = useState(false);
+    const [aLikeStatus, setALikedStatus] = useState(false);
+    const [bLikeStatus, setBLikedStatus] = useState(false);
+    const [aVotes, setAVotes] = useState();
+    const [bVotes, setBVotes] = useState();
 
     useEffect(() => {
+        setAVotes(props.aVotes);
+        setBVotes(props.bVotes);
+
         if({user}.user != null){
-            console.log(user);
+            if(props.aLikes){
+                setALikedStatus(props.aLikes.includes(JSON.parse({user}.user).id))
+            }
+
+            if(props.bLikes){
+                setBLikedStatus(props.bLikes.includes(JSON.parse({user}.user).id))
+            }
         }
-    }, [user]);
+    }, [user, props.aLikes, props.bLikes]);
 
     function vote(option, id) {
         let newPromptVote = {
@@ -25,7 +37,21 @@ function Voting(props) {
         return axios
             .post('http://localhost:5000/votePrompt', newPromptVote, {withCredentials: true})
             .then((res) => {
-                console.log(res);
+                if(option == 0){
+                    setALikedStatus(!aLikeStatus);
+                    aLikeStatus ? setAVotes(aVotes - 1) : setAVotes(aVotes + 1);
+                    if(bLikeStatus){
+                        setBVotes(bVotes - 1);
+                        setBLikedStatus(false);
+                    }
+                } else {
+                    setBLikedStatus(!bLikeStatus);
+                    bLikeStatus ? setBVotes(bVotes - 1) : setBVotes(bVotes + 1);
+                    if(aLikeStatus){
+                        setAVotes(aVotes - 1);
+                        setALikedStatus(false);
+                    }
+                }
             })
             .catch((err) => console.log(err));
     }
@@ -38,16 +64,16 @@ function Voting(props) {
         </div>
         <div className="d-flex justify-content-evenly">
             <div>
-                <Button variant="secondary" onClick={() => vote(0, props.promptId)}>
+                <Button variant={aLikeStatus ? "primary" : "secondary"} onClick={() => vote(0, props.promptId)}>
                     {props.aChoice}
                 </Button>
-                <p className="votes">Votes: {props.aVotes}</p>
+                <p className="votes">Votes: {aVotes}</p>
             </div>
             <div>
-                <Button variant="secondary" onClick={() => vote(1, props.promptId)}>
+                <Button variant={bLikeStatus ? "primary" : "secondary"} onClick={() => vote(1, props.promptId)}>
                     {props.bChoice}
                 </Button>
-                <p className="votes">Votes: {props.bVotes}</p>
+                <p className="votes">Votes: {bVotes}</p>
             </div>
         </div>
     </div>
