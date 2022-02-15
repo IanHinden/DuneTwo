94f00a6cd84df21b0cfc6b661ca15cb658ebe8c7
Ian <Ian.Hinden@gmail.com>
2022-02-14 23:27:42 -0800

    Changed base url in twitter login to environment variable

--- passport.js
+++ passport.js
@@ -69,7 +69,7 @@ passport.use(
 passport.use(new TwitterStrategy({
     consumerKey: process.env.TWITTER_API_KEY,
     consumerSecret: process.env.TWITTER_API_SECRET,
-    callbackURL: 'http://localhost:5000/auth/twitter/callback',
+    callbackURL: `${process.env.BASE_URL}auth/twitter/callback`,
     userProfileURL: 'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
     includeEmail: true,
   },
